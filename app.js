0c7e435de1f13833608bed8823d941519be933c5
RAYBADU <raymondafrani00@gmail.com>
2025-08-25 10:39:13 +0000

    added some interactivity

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const hamBtn = document.getElementById("ham-btn");
 const menu = document.getElementById("menu");
-const closeBtn = document.getElementById("close-btn")
+const closeBtn = document.getElementById("close-btn");
+const orderBtn = document.getElementById("order-btn");
+const modal = document.getElementById("modal");
+const body = document.getElementById("body")
 
 hamBtn.addEventListener("click", () => {
   menu.classList.toggle("hidden");
+  modal.style.display = "none"
+  body.classList.add("overflow-hidden")
 });
 
-closeBtn.addEventListener("click",()=>{
-      menu.classList.toggle("hidden");
+closeBtn.addEventListener("click", () => {
+  menu.classList.toggle("hidden");
+});
 
+orderBtn.addEventListener("click", ()=>{
+  modal.classList.toggle("hidden")
+  // body.style.filter = "blur(5px)"
+  
 })
